970ecb1e76cb5442416987ec4f5a1ecc11ebe3d0
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2021-01-30 17:15:51 +0500

    Update results

--- experiments/client.sh
+++ experiments/client.sh
@@ -4,5 +4,8 @@ for i in {1..50}
 do
 	echo "Doing experiment number $i";
 	#iperf -c $1 -p 443
-	iperf -c $1 -p 80
+	#iperf -c $1 -p 80
+	time wget https://mirrors.edge.kernel.org/pub/linux/kernel/v1.0/linux-1.0.tar.gz 2>/dev/null 1> tmp
+	cat tmp | grep real
+	rm linux-1.0.tar.gz
 done
--- experiments/throughput.R
+++ experiments/throughput.R
@@ -1,9 +1,11 @@
 vpn<-scan("results_TLS_VPN.dat");
 no_vpn<-scan("results_NO_VPN.dat");
+no_vpn<-1.3*8/no_vpn*1024;
+vpn<-1.3*8/vpn*1024;
 summary(vpn);
 summary(no_vpn);
 pdf("throughput.pdf", height=4);
-plot(ecdf(vpn), verticals=T, do.points=F, main="Cumulative distribution of throughputs", xlab="Throughput, Kb/s", ylab="Probability", lwd=3, col="dark blue");
+plot(ecdf(vpn), verticals=T, do.points=F, xlim=c(0, 2100), main="Cumulative distribution of throughputs", xlab="Throughput, Kb/s", ylab="Probability", lwd=3, col="dark blue");
 plot(ecdf(no_vpn), lwd=3, col="dark red", add=T, verticals=T, do.points=F);
 grid(col="black");
 legend("bottomright", c("TLS VPN", "Plain TCP"), col=c("dark blue", "dark red"), bty="n", lwd=c(3, 3))
