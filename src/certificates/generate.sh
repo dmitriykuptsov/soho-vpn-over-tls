86abbb189662ba8a598f2684b3a732887da1da2c
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2021-01-31 15:26:44 +0500

    Update certificate generator

--- src/certificates/generate.sh
+++ src/certificates/generate.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-
-# Read certificate chain contents
-openssl x509 -in certchain.pem -text
-
 # Generate new self-signed certificate
-openssl req -new -x509 -days 365 -nodes -out cert.pem -keyout cert.pem
+openssl req -new -x509 -days 365 -nodes -out certchain.pem -keyout private.key
