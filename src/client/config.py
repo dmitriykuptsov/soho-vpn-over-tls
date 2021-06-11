d0096493fa2f1e386030dd23d208abb0f34e380c
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2021-06-11 21:50:38 +0500

    Update config.py
    Thanks to Joachim Bauch new options now available!

--- src/client/config.py
+++ src/client/config.py
@@ -1,12 +1,12 @@
 config = {
-	"SERVER_IP": "94.237.31.77",
+	"SERVER_IP": "94.237.31.77", # Optional, if SERVER_HOSTNAME is registered in the DNS
 	"SERVER_PORT": 443,
 	"USERNAME": "dmitriy",
 	"PASSWORD": "test",
 	"TUN_NAME": "tun1",
 	"SERVER_HOSTNAME": "strangebit.com",
-	"CA_CERTIFICATE": "./certificates/certchain.pem",
+	"CA_CERTIFICATE": "./certificates/certchain.pem", #Optional, if trusted certificates are used
 	"BUFFER_SIZE": 1500,
-	"DEFAULT_GW": "10.0.2.2",
+	"DEFAULT_GW": "10.0.2.2", # Optional, now default gateway is obtained from /proc/route file
 	"DNS_SERVER": "8.8.8.8"
 }
