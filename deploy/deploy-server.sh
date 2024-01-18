3727c852f99f697e6d155b2b552e4f0f7b840f95
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2024-01-18 16:04:47 +0500

    Added server side deployment script

---
+++ deploy/deploy-server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir /opt/vpn/
+rsync -rv src/* /opt/vpn/
+sudo rsync -rv system.d/vpn-server.service /etc/systemd/system
+sudo systemctl enable vvpn-server
+sudo systemctl daemon-reload
+
