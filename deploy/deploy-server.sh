3085060d0fe4e1521fb5156b133acdb2ed6f3af0
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2024-01-18 16:22:13 +0500

    Fixed typo

--- deploy/deploy-server.sh
+++ deploy/deploy-server.sh
@@ -3,7 +3,7 @@
 mkdir /opt/vpn/
 rsync -rv src/* /opt/vpn/
 sudo rsync -rv system.d/vpn-server.service /etc/systemd/system
-sudo systemctl enable vvpn-server
+sudo systemctl enable vpn-server
 sudo systemctl daemon-reload
 
 
